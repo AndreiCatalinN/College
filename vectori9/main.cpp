2122b674df0621939299820f7afeeeedc69089f0
Andrei Catalin Negura <c16733435@mydit.ie>
2019-07-29 15:39:04 +0100

    first commit

---
+++ vectori9/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+ifstream f("date.in");
+
+int main()
+{
+    int n,i,v[101], m=-10000,a,b,y;
+    f>>n;
+    for(i=1; i<=n; i++)
+        f>>v[i];
+    for(i=1; i<n; i++)
+    {
+        y=v[i]+v[i+1];
+        if(m<y)
+        {
+            m=y;
+            a=v[i];
+            b=v[i+1];
+        }
+        else if(m==y)
+
+        }
+    cout<<a<<"+"<<b<<"="<<m<<endl;
+    f.close();
+    return 0;
+}
